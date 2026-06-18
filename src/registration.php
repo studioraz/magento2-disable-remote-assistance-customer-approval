6e4533b872a6588e0af3358b08f0f3b3fed5f332
Itay Raz <itay@studioraz.co.il>
2026-06-18 21:28:38 +0300

    feat(module): rename and update DisableRemoteAssitance module
    * Renamed module to DisableCustomerRemoteAssistanceConsent
    * Updated resource IDs and descriptions in acl.xml, menu.xml, module.xml, and registration.php

--- src/registration.php
+++ src/registration.php
@@ -8,6 +8,6 @@ use \Magento\Framework\Component\ComponentRegistrar;
 
 ComponentRegistrar::register(
     ComponentRegistrar::MODULE,
-    'SR_DisableRemoteAssitance',
+    'SR_DisableCustomerRemoteAssistanceConsent',
     __DIR__
 );
